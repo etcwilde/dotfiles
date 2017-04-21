8982df398936110e1e72a1a96312a1e8aac81e02
Evan Wilde <etcwilde@uvic.ca>
2017-04-21 13:04:14 -0700

    initial zsh setup, aliases, and history configuration
    This is the initial zsh setup.

---
+++ zsh/aliases.zsh
@@ -0,0 +1,12 @@
+# Emacs aliases
+alias e="emacsclient -t"
+alias ec="emacsclient -c"
+alias em="emacs -t"
+
+# Basic Environment
+alias open="xdg-open"
+alias ls="ls --color=auto"
+alias la="ls -A"
+
+# Watch with color
+alias watch="watch --color"
