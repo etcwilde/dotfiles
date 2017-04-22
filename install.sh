56b84c1e66470fe955cf717b881a148d70c2fbe7
Evan Wilde <etcwilde@uvic.ca>
2017-04-21 17:32:13 -0700

    adding tmux installation file

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@
 # vim:set et sw=2 ts=4 tw=84:
 # Installs my dotfiles quickly and efficiently
 # Evan Wilde      <etcwilde@uvic.ca>
+
+ln --backup -s "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
+
+CONFIG="$HOME/.config"
+
+ln --backup -s "$PWD/tmux" "$CONFIG"
