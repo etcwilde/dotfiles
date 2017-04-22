1dd691d021d25908e4f6d3a545c4df4ea5517abe
Evan Wilde <etcwilde@uvic.ca>
2017-04-21 17:34:50 -0700

    adding i3 installation

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@
 # vim:set et sw=2 ts=4 tw=84:
 # Installs my dotfiles quickly and efficiently
 # Evan Wilde      <etcwilde@uvic.ca>
+
+if [ -d "$HOME/.i3" ]; then
+  echo -e "\033[0;33mWarning:\033[0m i3 (~/.i3) folder already exits, create the symbolic link manually"
+else
+  ln --backup -s "$PWD/git/template" "$HOME/.i3"
+fi
