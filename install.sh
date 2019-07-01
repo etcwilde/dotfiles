1c5eb4f4c6edd4bb5672cb1496c8152913923916
Evan Wilde <etceterawilde@gmail.com>
2019-06-30 19:47:50 -0700

    Updating install script
    Updating my install script. I don't have a git template directory
    anymore, so we don't need that step, and i3 and termite configs only
    need to be installed on Linux.

--- install.sh
+++ install.sh
@@ -1,31 +1,28 @@
-#!/bin/sh
-# vim:set et sw=2 ts=4 tw=84:
-# Installs my dotfiles quickly and efficiently
-# Evan Wilde      <etcwilde@uvic.ca>
+#!/bin/zsh
+# vim:set et sw=2 ts=2 tw=84:
+# Install my dotfiles quickly
+# Evan Wilde      <etceterawilde@gmail.com>
 
 ln --backup -s "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
 ln --backup -s "$PWD/zsh/zshrc" "$HOME/.zshrc"
 ln --backup -s "$PWD/git/config" "$HOME/.gitconfig"
 ln --backup -s "$PWD/git/gitignore" "$HOME/.gitignore"
 
-if [ -d "$HOME/.i3" ]; then
-  echo -e "\033[0;33mWarning:\033[0m i3 (~/.i3) folder already exits, create the symbolic link manually"
-else
-  ln --backup -s "$PWD/i3" "$HOME/.i3"
+if [[ $(uname -s) == 'Linux' ]]; then
+  if [[ -d "$HOME/.i3" ]]; then
+    echo -e "\033[0;33mWarning:\033[0m i3 (~/.i3) folder already exits, create the symbolic link manually"
+  else
+    ln -s "$PWD/i3" "$HOME/.i3"
+  fi
 fi
-
-if [ -d "$HOME/.git" ]; then
-  echo -e "\033[0;33mWarning:\033[0m Git (~/.git) folder already exits, create the symbolic link manually"
-else
-  ln --backup -s "$PWD/git/template" "$HOME/.git"
-fi
-
 
 CONFIG="$HOME/.config"
 
 ln --backup -s "$PWD/zsh" "$CONFIG"
-ln --backup -s "$PWD/termite" "$CONFIG"
 ln --backup -s "$PWD/tmux" "$CONFIG"
 ln --backup -s "$PWD/nvim" "$CONFIG"
 
-xrdb -merge "$PWD/Xresources"
+if [[ $(uname -s) == 'Linux' ]]; then
+  ln --backup -s "$PWD/termite" "$CONFIG"
+  xrdb -merge "$PWD/Xresources"
+fi
