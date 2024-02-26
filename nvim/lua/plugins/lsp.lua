477c4a33f9c082e7583372e29e3c7837d8493616
Evan Wilde <etceterawilde@gmail.com>
2024-02-26 11:40:19 -0800

    [nvim] clean up lsp.lua indentation
    Not sure how I screwed that up as much as I did. Some lines were
    single-width indented, while others were two-wide. Made that consistent.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -18,46 +18,45 @@ return {
         ["txtdocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, borders),
       }
 
-     local servers = {
-       clangd = {},
-       sourcekit = {
-         root_dir = lspconfig.util.root_pattern(
-           '.git',
-           'Package.swift',
-           'compile_commands.json'
-         ),
-         capabilities = sklsp_register_cap()
-       },
-       lspconfig.rust_analyzer.setup {}
-     }
+    local servers = {
+      clangd = {},
+      sourcekit = {
+        root_dir = lspconfig.util.root_pattern(
+          '.git',
+          'Package.swift',
+          'compile_commands.json'
+        ),
+        capabilities = sklsp_register_cap()
+      },
+      lspconfig.rust_analyzer.setup {}
+    }
 
-     for server, setup in pairs(servers) do
-       setup.handlers = handlers
-       lspconfig[server].setup(setup)
-     end
+    for server, setup in pairs(servers) do
+      setup.handlers = handlers
+      lspconfig[server].setup(setup)
+    end
 
-     vim.api.nvim_create_autocmd('LspAttach', {
-       desc = 'LSP Actions',
-       callback = function(args)
-         -- Once we've attached, configure the keybindings
-         local wk = require('which-key')
-         wk.register({
-           K = {"<cmd>lua vim.lsp.buf.hover()<cr>", "LSP hover info"},
-           gd = {"<cmd>lua vim.lsp.buf.definition()<cr>", "LSP go to definition"},
-           gD = {"<cmd>lua vim.lsp.buf.declaration()<cr>", "LSP go to declaration"},
-           gi = {"<cmd>lua vim.lsp.buf.implementation()<cr>", "LSP go to implementation"},
-           gr = {"<cmd>lua vim.lsp.buf.references()<cr>", "LSP list references"},
-           gs = {"<cmd>lua vim.lsp.buf.signature_help()<cr>", "LSP signature help"},
-           gn = {"<cmd>lua vim.lsp.buf.rename()<cr>", "LSP rename"},
-           ["[g"] = {"<cmd>lua vim.diagnostic.goto_prev()<cr>", "Go to previous diagnostic"},
-           ["g]"] = {"<cmd>lua vim.diagnostic.goto_next()<cr>", "Go to next diagnostic"},
-         }, {
-           mode = 'n',
-           -- buffer = true,
-           silent = true,
-         })
-       end,
-     })
-   end
+    vim.api.nvim_create_autocmd('LspAttach', {
+      desc = 'LSP Actions',
+      callback = function(args)
+        -- Once we've attached, configure the keybindings
+        local wk = require('which-key')
+        wk.register({
+          K = {"<cmd>lua vim.lsp.buf.hover()<cr>", "LSP hover info"},
+          gd = {"<cmd>lua vim.lsp.buf.definition()<cr>", "LSP go to definition"},
+          gD = {"<cmd>lua vim.lsp.buf.declaration()<cr>", "LSP go to declaration"},
+          gi = {"<cmd>lua vim.lsp.buf.implementation()<cr>", "LSP go to implementation"},
+          gr = {"<cmd>lua vim.lsp.buf.references()<cr>", "LSP list references"},
+          gs = {"<cmd>lua vim.lsp.buf.signature_help()<cr>", "LSP signature help"},
+          gn = {"<cmd>lua vim.lsp.buf.rename()<cr>", "LSP rename"},
+          ["[g"] = {"<cmd>lua vim.diagnostic.goto_prev()<cr>", "Go to previous diagnostic"},
+          ["g]"] = {"<cmd>lua vim.diagnostic.goto_next()<cr>", "Go to next diagnostic"},
+        }, {
+          mode = 'n',
+          silent = true,
+        })
+      end,
+    })
+    end
   }
 }
